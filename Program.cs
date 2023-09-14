e378541dd8da8f54add457def65ec5158d089959
Danielsilva200319 <89894235+Danielsilva200319@users.noreply.github.com>
2023-09-14 13:06:24 -0500

    r

--- Program.cs
+++ Program.cs
@@ -8,20 +8,20 @@ internal class Program
         int?[] args2 = new int?[5];
         Console.WriteLine(args2[0]);
         LinQueries queries = new LinQueries();
-        imprimirValores(queries.AllCollection());
-        imprimirValores(queries.LibrosAndroid());
-        imprimirValores(queries.LibrosDespues2000());
-        imprimirValores(queries.LibrosAndroid2005());
-        imprimirValores(queries.Libros250paginas());
-        Console.WriteLine(queries.verificar() ? "Todos los libros contienen un status" : "Al menos uno de los libros no contiene un status");
-        Console.WriteLine(queries.verificar2005() ? "Al menos un libro es de 2005" : "No hay libros");
+        // imprimirValores(queries.AllCollection());
+        // imprimirValores(queries.LibrosAndroid());
+        // imprimirValores(queries.LibrosDespues2000());
+        // imprimirValores(queries.LibrosAndroid2005());
+        // imprimirValores(queries.Libros250paginas());
+        // Console.WriteLine(queries.verificar() ? "Todos los libros contienen un status" : "Al menos uno de los libros no contiene un status");
+        // Console.WriteLine(queries.verificar2005() ? "Hay libros de 2005" : "No hay libros");
     }
     private static void imprimirValores(IEnumerable<Book> books)
     {
         int registros = 0;
         Console.Clear();
         Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine("{0,-70} {1,7} {2,20}", "N. Paginas", "Fecha Publicaación");
+        Console.WriteLine("{0,-70} {1,7} {2,20}", "titulo", "N. Paginas", "Fecha Publicaación");
         foreach (Book book in books)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
