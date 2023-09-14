892d98c328630b049cd7438dd339cf35a9a2d5fd
Danielsilva200319 <89894235+Danielsilva200319@users.noreply.github.com>
2023-09-14 12:42:38 -0500

    first commit

---
+++ Entities/LinQueries.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practicaLinQ;
+public class LinQueries
+{
+    List<Book> listBooks = new List<Book>();
+    public LinQueries()
+    {
+        using (StreamReader reader = new StreamReader("books.json"))
+        {
+            string json = reader.ReadToEnd();
+            this.listBooks = System.Text.Json.JsonSerializer
+            .Deserialize<List<Book>>(json,new System.Text.Json.JsonSerializerOptions() 
+            { PropertyNameCaseInsensitive = true }) ?? new List<Book>();
+        }
+    }
+    public IEnumerable<Book> AllCollection()
+    {
+        return listBooks;
+    }
+    public IEnumerable<Book> LibrosDespues2000()
+    {
+        return from book in listBooks where book.PublishedDate.Year >200 select book;
+    }
+    public IEnumerable<Book> LibrosAndroid()
+    {
+        return from book in listBooks where book.Title.Contains("Android") select book;
+    }
+    public IEnumerable<Book> LibrosAndroid2005()
+    {
+        return from book in listBooks where book.PublishedDate.Year > 2005 && book.Title.Contains("Android") select book;
+    }
+    public IEnumerable<Book> Libros250paginas()
+    {
+        return from book in listBooks where book.PageCount > 250 && book.Title.Contains("Action") select book;
+    }
+    public bool verificar()
+    {
+        return listBooks.All(book => book.Status != String.Empty);
+    }
+    public bool verificar2005()
+    {
+        return listBooks.Any(book => book.PublishedDate.Year == 2005);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+using practicaLinQ;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int?[] args2 = new int?[5];
+        Console.WriteLine(args2[0]);
+        LinQueries queries = new LinQueries();
+        imprimirValores(queries.AllCollection());
+        imprimirValores(queries.LibrosAndroid());
+        imprimirValores(queries.LibrosDespues2000());
+        imprimirValores(queries.LibrosAndroid2005());
+        imprimirValores(queries.Libros250paginas());
+        Console.WriteLine(queries.verificar() ? "Todos los libros contienen un status" : "Al menos uno de los libros no contiene un status");
+        Console.WriteLine(queries.verificar2005() ? "Al menos un libro es de 2005" : "Ninguno de los libros es de 2005");
+    }
+    private static void imprimirValores(IEnumerable<Book> books)
+    {
+        int registros = 0;
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("{0,-70} {1,7} {2,20}", "N. Paginas", "Fecha Publicaación");
+        foreach (Book book in books)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            registros += 1;
+            Console.WriteLine("{0,-70} {1,7} {2,20}", book.Title, book.PageCount, book.PublishedDate.ToShortDateString());
+        }
+    }
+}
